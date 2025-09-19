eb09e9b5e939e3642da72d1973236a98ca51153c
Geoff <dafaakaa@gmail.com>
2025-09-19 21:27:08 +0800

    Initial Next Project002

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // <-- this makes it export as static HTML
+  images: {
+    unoptimized: true, // needed for GitHub Pages
+  },
+  basePath: '/project002', // <-- use your repo name here
+  assetPrefix: "/project002/", // <-- Add this for correct asset paths
 };
 
 export default nextConfig;
